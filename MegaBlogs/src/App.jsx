d502f2d9eb1afac575464cce1815e2635f3feef4
arvindkumar1000 <arvindnagar58955@gmail.com>
2025-10-29 14:58:53 +0530

    complete header and footer and input button

--- MegaBlogs/src/App.jsx
+++ MegaBlogs/src/App.jsx
@@ -1,16 +1,45 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import { Footer, Header } from './components/Index'
+import {Outlet} from 'react-router-dom'
 
 function App() {
 
-console.log(import.meta.env.VITE_APPWRITE_URL);
+// console.log(import.meta.env.VITE_APPWRITE_URL);
+const [loading,setLoading]=useState(true)
+const dispatch=useDispatch()
+useEffect(()=>{
+  authService.getCurrentUser()
+  .then((userData)=>{
+    if (userData) {
+      dispatch(login({userData}))
+    }else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=>setLoading(false))
+},)
 
-  return (
-    <>
-     <h1>blog with appwrite</h1>
-    
-    </>
-  )
+
+return !loading?(
+<div className='min-h-sc flex flex-wrap content-between bg-gray-400'>
+
+<div className='w-full block'>
+  <Header/>
+  <main>
+    Todo:{/* <Outlet /> */}
+  </main>
+  <Footer/>
+</div>
+
+</div>)
+:null
+
 }
 
 export default App
